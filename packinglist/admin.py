547a295ae7a8a858060680b81434265f2f71d0f1
TeodoraAlina <>
2023-07-15 19:48:40 +0000

    Create get_packing_list function in views.py

--- packinglist/admin.py
+++ packinglist/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from .models import PackingList
-from .models import Task
+
 
 admin.site.register(PackingList)
-admin.site.register(Task)
--- packinglist/views.py
+++ packinglist/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import PackingList
+from .models import Task
 
-# Create your views here.
+
+def get_packing_list(request):
+    packinglists = PackingList.objects.all()
+    context = {
+        'packinglists': packinglists
+    }
+    return render(request, 'packinglist.html', context)
