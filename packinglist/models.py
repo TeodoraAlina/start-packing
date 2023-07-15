297b08a5dc54fff731e22476c4ba0751ebfe4e79
TeodoraAlina <>
2023-07-15 15:11:23 +0000

    Create PackingList and Task models to models.py

--- packinglist/models.py
+++ packinglist/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class PackingList(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=70)
+    created_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    packing_list = models.ForeignKey(PackingList, on_delete=models.CASCADE)
+    completed = models.BooleanField(default=False)
