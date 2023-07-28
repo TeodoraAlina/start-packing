0b9e2d3cf3bb433a28d612e094d3c03d5555aa47
TeodoraAlina <>
2023-07-28 06:23:32 +0000

    Updated model Task with related_name and template references in packinglist.html

--- packinglist/models.py
+++ packinglist/models.py
@@ -14,7 +14,8 @@ class PackingList(models.Model):
 
 class Task(models.Model):
     title = models.CharField(max_length=200)
-    packing_list = models.ForeignKey(PackingList, on_delete=models.CASCADE)
+    packing_list = models.ForeignKey(
+        PackingList, on_delete=models.CASCADE, related_name="tasks")
     completed = models.BooleanField(default=False)
 
     def __str__(self):
