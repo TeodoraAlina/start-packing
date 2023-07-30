c4358d8f0c4591bb2cfb1264a439d994b2367a11
TeodoraAlina <>
2023-07-30 08:14:20 +0000

    Added created_date to Packinglist and created_task_date to Task models

--- packinglist/models.py
+++ packinglist/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 
 class PackingList(models.Model):
     user = models.ForeignKey(
         User, on_delete=models.CASCADE, null=False)
     title = models.CharField(max_length=70)
-    created_date = models.DateField(auto_now_add=True)
+    created_date = models.DateField(default=timezone.now)
 
     def __str__(self):
         return self.title
@@ -16,6 +17,7 @@ class Task(models.Model):
     title = models.CharField(max_length=200)
     packing_list = models.ForeignKey(
         PackingList, on_delete=models.CASCADE, related_name="tasks")
+    created_task_date = models.DateField(default=timezone.now)
     completed = models.BooleanField(default=False)
 
     def __str__(self):
